a02d7b3b48913fae00f0e32dff05151e88de6cd8
AramisLins <122841235+AramisLins@users.noreply.github.com>
2023-01-21 21:52:15 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const botaoTrailer = document.querySelector('.botao-trailer');
+const botaofecharModal = document.querySelector('.fechar-modal');
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src ;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaofecharModal .addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
+
+
